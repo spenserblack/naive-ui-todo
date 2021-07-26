9ff96621e0ad4c9034c82dbb0c0eec44e8a3aa26
Spenser Black <spenserblack01@gmail.com>
2021-07-26 11:10:05 -0400

    Define store mutations
    Store mutations defined for creating/editing todo lists and their items.
    Because tests for mutations required expressions that ESLint detects as
    unused, override added in `tests` to allow unused expressions.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { InjectionKey } from 'vue';
+import { InjectionKey, reactive } from 'vue';
 import { createStore, useStore as baseUseStore, Store } from 'vuex';
 
 export interface TodoItem {
@@ -17,12 +17,49 @@ export interface State {
 
 export const key: InjectionKey<Store<State>> = Symbol('store');
 
+export const mutations = {
+  load: (state: State, todos: Array<TodoList>): void => {
+    state.todos = reactive(todos);
+  },
+  addList: (state: State): void => {
+    state.todos.push({ title: '', items: [] });
+  },
+  removeList: (state: State, index: number): void => {
+    state.todos.splice(index, 1);
+  },
+  setTodoTitle: (state: State, { index, title }: { index: number, title: string }): void => {
+    state.todos[index].title = title;
+  },
+  addTodoItem: (state: State, { index }: { index: number }): void => {
+    state.todos[index].items.push({ description: '', done: false });
+  },
+  removeTodoItem: (state: State, { todoIndex, itemIndex }: {
+    todoIndex: number, itemIndex: number
+  }): void => {
+    state.todos[todoIndex].items.splice(itemIndex, 1);
+  },
+  setTodoItemDescription: (state: State, { todoIndex, itemIndex, description }: {
+    todoIndex: number, itemIndex: number, description: string
+  }): void => {
+    state.todos[todoIndex].items[itemIndex].description = description;
+  },
+  completeTodoItem: (state: State, { todoIndex, itemIndex }: {
+    todoIndex: number, itemIndex: number
+  }): void => {
+    state.todos[todoIndex].items[itemIndex].done = true;
+  },
+  startTodoItem: (state: State, { todoIndex, itemIndex }: {
+    todoIndex: number, itemIndex: number
+  }): void => {
+    state.todos[todoIndex].items[itemIndex].done = false;
+  },
+};
+
 export const store = createStore<State>({
   state: {
     todos: [],
   },
-  mutations: {
-  },
+  mutations,
   actions: {
   },
   modules: {
---
+++ tests/unit/store/mutations.spec.ts
@@ -0,0 +1,132 @@
+import { expect } from 'chai';
+import { mutations } from '@/store';
+
+describe('store', () => {
+  describe('mutations', () => {
+    describe('load', () => {
+      const { load } = mutations;
+
+      it('should load a new state for to-do lists', () => {
+        const state = { todos: [{ title: 'foo', items: [{ description: 'x', done: true }] }] };
+
+        load(state, [{ title: 'bar', items: [{ description: 'y', done: false }] }]);
+
+        expect(state.todos).to.be.an('array').with.lengthOf(1);
+
+        const todo = state.todos[0];
+        expect(todo.title).to.equal('bar');
+        expect(todo.items).to.be.an('array').with.lengthOf(1);
+
+        const item = todo.items[0];
+        expect(item.description).to.equal('y');
+        expect(item.done).to.be.false;
+      });
+    });
+    describe('addList', () => {
+      const { addList } = mutations;
+      it('should add an empty list', () => {
+        const state = { todos: [] };
+
+        addList(state);
+
+        expect(state.todos).to.have.lengthOf(1);
+      });
+    });
+
+    describe('removeList', () => {
+      const { removeList } = mutations;
+
+      it('should remove a to-do list', () => {
+        const state = {
+          todos: [
+            { title: 'foo', items: [] },
+            { title: 'bar', items: [] },
+          ],
+        };
+
+        removeList(state, 0);
+
+        expect(state.todos).to.have.lengthOf(1);
+        expect(state.todos[0].title).to.equal('bar');
+      });
+    });
+
+    describe('setTodoTitle', () => {
+      const { setTodoTitle } = mutations;
+
+      it('should set title of a to-do list', () => {
+        const state = { todos: [{ title: 'foo', items: [] }] };
+
+        setTodoTitle(state, { index: 0, title: 'bar' });
+
+        expect(state.todos[0].title).to.equal('bar');
+      });
+    });
+
+    describe('addTodoItem', () => {
+      const { addTodoItem } = mutations;
+
+      it('should add a to-do list item', () => {
+        const state = { todos: [{ title: 'foo', items: [] }] };
+
+        addTodoItem(state, { index: 0 });
+
+        expect(state.todos[0].items).have.lengthOf(1);
+      });
+    });
+
+    describe('removeTodoItem', () => {
+      const { removeTodoItem } = mutations;
+
+      it('should remove a to-do list item', () => {
+        const state = { todos: [{ title: 'foo', items: [{ description: 'bar', done: true }, { description: 'baz', done: false }] }] };
+
+        removeTodoItem(state, { todoIndex: 0, itemIndex: 0 });
+
+        const { items } = state.todos[0];
+        expect(items).have.lengthOf(1);
+        expect(items[0].description).to.equal('baz');
+        expect(items[0].done).to.be.false;
+      });
+    });
+
+    describe('setTodoItemDescription', () => {
+      const { setTodoItemDescription } = mutations;
+
+      it('should set the title of a to-do list item', () => {
+        const state = { todos: [{ title: 'foo', items: [{ description: 'bar', done: true }] }] };
+
+        setTodoItemDescription(state, { todoIndex: 0, itemIndex: 0, description: 'baz' });
+
+        const { description } = state.todos[0].items[0];
+        expect(description).to.equal('baz');
+      });
+    });
+
+    describe('completeTodoItem', () => {
+      const { completeTodoItem } = mutations;
+
+      it('should mark an item as complete', () => {
+        const state = { todos: [{ title: 'foo', items: [{ description: 'bar', done: false }] }] };
+
+        completeTodoItem(state, { todoIndex: 0, itemIndex: 0 });
+
+        const { done } = state.todos[0].items[0];
+        expect(done).to.be.true;
+      });
+    });
+
+    describe('startTodoItem', () => {
+      const { startTodoItem } = mutations;
+
+      it('should mark an item as incomplete', () => {
+        const state = { todos: [{ title: 'foo', items: [{ description: 'bar', done: true }] }] };
+
+        startTodoItem(state, { todoIndex: 0, itemIndex: 0 });
+
+        const { done } = state.todos[0].items[0];
+        expect(done).to.be.false;
+      });
+    });
+  });
+});
