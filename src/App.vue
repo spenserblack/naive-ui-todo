2240c1e18a8a2fdb17e2fbc940aaacb7cb30a287
Spenser Black <spenserblack01@gmail.com>
2021-08-03 13:47:32 -0400

    Watch to-do array for autosave
    Watches not only when values within the to-do list change, but also when
    a to-do list is added or deleted.

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,7 @@ export default defineComponent({
         save();
       }
     }, 1000);
-    watch([...todos.value], saveIfAutosave);
+    watch([todos.value, ...todos.value], saveIfAutosave);
     watch(saveAutomatically, () => saveAutoSave(saveAutomatically.value));
 
     return {
