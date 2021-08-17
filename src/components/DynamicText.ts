3de9740bc716499b8f9a2dca539f8096ada271cb
Spenser Black <spenserblack01@gmail.com>
2021-08-17 11:54:30 -0400

    Use `h` function instead of .tsx
    Slightly more readable. Also seems to have much stricter type checking
    on the rendered component.

--- src/components/DynamicText.ts
+++ src/components/DynamicText.ts
@@ -1,6 +1,6 @@
 import {
   defineComponent,
-  renderSlot,
+  h,
   PropType,
 } from 'vue';
 import {
@@ -47,17 +47,14 @@ export const depthProp = {
   default: 1,
 };
 
-type TextComponents = typeof NH1 | typeof NH2 | typeof NH3 | typeof NH4 | typeof NH5 | typeof NH6 |
-  typeof NP | typeof NText;
-const componentRecord: Record<TagProp, TextComponents> = {
+type TextComponents = typeof NH1 | typeof NH2 | typeof NH3 | typeof NH4 | typeof NH5 | typeof NH6;
+const headerRecord: Record<Exclude<TagProp, 'p' | 'text'>, TextComponents> = {
   h1: NH1,
   h2: NH2,
   h3: NH3,
   h4: NH4,
   h5: NH5,
   h6: NH6,
-  p: NP,
-  text: NText,
 };
 
 export default defineComponent({
@@ -72,20 +69,29 @@ export default defineComponent({
     code: codeProp,
   },
   render() {
-    const defaultSlot = renderSlot(this.$slots, 'default');
-    const TextComponent = componentRecord[this.tag];
+    const {
+      depth, strong, italic, underline, code,
+    } = this;
+    const { default: defaultSlot } = this.$slots;
 
-    return (
-      <TextComponent
-        depth={this.depth}
-        strong={this.strong}
-        italic={this.italic}
-        underline={this.underline}
-        delete={this.delete}
-        code={this.code}
-      >
-        {defaultSlot}
-      </TextComponent>
-    );
+    switch (this.tag) {
+      case 'text':
+        return h(
+          NText,
+          {
+            strong,
+            italic,
+            underline,
+            delete: this.delete,
+            code,
+            depth,
+          },
+          defaultSlot,
+        );
+      case 'p':
+        return h(NP, { depth }, defaultSlot);
+      default:
+    }
+    return h(headerRecord[this.tag], null, defaultSlot);
   },
 });
