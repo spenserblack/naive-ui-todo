a8cdb5f4ab77c95e40826fa56215dfd3d4305569
Spenser Black <spenserblack01@gmail.com>
2021-08-18 11:55:37 -0400

    Use setup script for to-do list view

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,33 +1,21 @@
-<template lang="pug">
-.todo
-  TodoList(:index="index" @delete="onDelete")
+<template>
+  <TodoList :index="index" @delete="onDelete" />
 </template>
 
-<script lang="ts">
-import { defineComponent, computed } from 'vue';
+<script setup lang="ts">
+import { computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useStore } from '@/store';
 import TodoList from '@/components/TodoList.vue';
 
-export default defineComponent({
-  name: 'Todo',
-  components: { TodoList },
+const route = useRoute();
+const router = useRouter();
+const store = useStore();
 
-  setup() {
-    const route = useRoute();
-    const router = useRouter();
-    const store = useStore();
-
-    const index = computed(() => Number.parseInt(route.params.index as string, 10));
-
-    return {
-      index,
-      onDelete: async (): Promise<void> => {
-        const i = index.value;
-        await router.replace({ name: 'Home' });
-        store.commit('removeList', i);
-      },
-    };
-  },
-});
+const index = computed(() => Number.parseInt(route.params.index as string, 10));
+const onDelete = async (): Promise<void> => {
+  const i = index.value;
+  await router.replace({ name: 'Home' });
+  store.commit('removeList', i);
+};
 </script>
