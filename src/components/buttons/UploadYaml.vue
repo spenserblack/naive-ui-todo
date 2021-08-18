f0c9f848348646e44f99d4395ac8e52d7a3c6211
Spenser Black <spenserblack01@gmail.com>
2021-08-18 10:08:00 -0400

    Use setup script for download button

--- src/components/buttons/UploadYaml.vue
+++ src/components/buttons/UploadYaml.vue
@@ -1,20 +1,20 @@
-<template lang="pug">
-NPopover(placement="top-end")
-  template(#trigger)
-    NUpload(
-      :accept="acceptTypes"
-      :showFileList="false"
-      @change="onUploadChange"
-    )
-      NButton(type="primary")
-        template(#icon)
-          NIcon: UploadIcon
-        slot Upload
-  | This will #[NText(italic) overwrite] the current to-do lists.
+<template>
+  <NPopover placement="top-end">
+    <span>This will <NText italic>overwrite</NText> the current to-do lists.</span>
+    <template #trigger>
+      <NUpload :accept="acceptTypes" :showFileList="false" @change="onUploadChange">
+        <NButton type="primary">
+          <template #icon>
+            <NIcon><UploadIcon /></NIcon>
+          </template>
+          <slot>Upload</slot>
+        </NButton>
+      </NUpload>
+    </template>
+  </NPopover>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
 import {
   NButton, NIcon, NPopover, NText, NUpload,
 } from 'naive-ui';
@@ -26,25 +26,17 @@ type OnChangeParam = {
   [key: string]: unknown,
 };
 
-export default defineComponent({
-  components: {
-    NButton, NIcon, NPopover, NText, NUpload, UploadIcon,
-  },
-  setup() {
-    const store = useStore();
+const store = useStore();
 
-    return {
-      onUploadChange: async ({ file: { file } }: OnChangeParam): Promise<void> => {
-        store.commit('loadFromYaml', await file.text());
-      },
-      acceptTypes: ['yml', 'yaml'].map((ext) => [
-        `.${ext}`,
-        `text/${ext}`,
-        `text/x-${ext}`,
-        `application/${ext}`,
-        `application/x-${ext}`,
-      ]).flat().join(','),
-    };
-  },
-});
+const onUploadChange = async ({ file: { file } }: OnChangeParam): Promise<void> => {
+  store.commit('loadFromYaml', await file.text());
+};
+
+const acceptTypes = ['yml', 'yaml'].map((ext) => [
+  `.${ext}`,
+  `text/${ext}`,
+  `text/x-${ext}`,
+  `application/${ext}`,
+  `application/x-${ext}`,
+]).flat().join(',');
 </script>
