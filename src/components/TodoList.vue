2dfaf880ed1ad99339f5932ceda0bd66e3fd28a9
Spenser Black <spenserblack01@gmail.com>
2021-08-18 11:43:44 -0400

    Remove pug template lang
    Interferes with a lot of checks on the template's variable usage.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,43 +1,56 @@
-<template lang="pug">
-NCard.todo(size="huge" hoverable)
-  template(#header)
-    NSpace.todo-header(justify="left")
-      EditableText(
-        :text="todo.title",
-        @update:value="setTitle"
-        tag="h2"
-        size="large"
-        inputPlaceholder="Please add a title"
-      )
-  template(#header-extra)
-    NPopconfirm(@positive-click="onDelete" placement="top-end")
-      template(#trigger)
-        NButton(size="large" type="warning")
-          template(#icon)
-            NIcon: DeleteIcon
-          | Delete
-      | This will delete the whole to-do list.
-  .items
-    Item(
-      v-for="item in incompleteItems"
-      :todoIndex="index"
-      :itemIndex="item.index"
-      @delete="removeTodoItem"
-      :key="`todo-${todo.id}-item-${item.id}-not-done`"
-    )
-    Item(
-      v-for="item in completeItems"
-      :todoIndex="index"
-      :itemIndex="item.index"
-      @delete="removeTodoItem"
-      :key="`todo-${todo.id}-item-${item.id}-done`"
-    )
-  template(#footer)
-    NSpace(justify="left")
-      NButton(type="primary" @click="addTodoItem")
-        template(#icon)
-          NIcon: AddIcon
-        | Add {{ todo.title }} Item
+<template>
+  <NCard size="huge" hoverable>
+    <template #header>
+      <NSpace justify="left">
+        <EditableText
+          :text="todo.title"
+          @update:value="setTitle"
+          tag="h2"
+          size="large"
+          inputPlaceholder="Please add a title"
+        />
+      </NSpace>
+    </template>
+    <template #header-extra>
+      <NPopconfirm @positive-click="onDelete" placement="top-end">
+        <template #trigger>
+          <NButton size="large" type="warning">
+            <template #icon>
+              <NIcon><DeleteIcon /></NIcon>
+            </template>
+            Delete
+          </NButton>
+        </template>
+        This will delete the whole to-do list.
+      </NPopconfirm>
+    </template>
+    <div class="items">
+      <Item
+        v-for="item in incompleteItems"
+        :todoIndex="index"
+        :itemIndex="item.index"
+        @delete="removeTodoItem"
+        :key="`todo-${todo.id}-item-${item.id}-not-done`"
+      />
+      <Item
+        v-for="item in completeItems"
+        :todoIndex="index"
+        :itemIndex="item.index"
+        @delete="removeTodoItem"
+        :key="`todo-${todo.id}-item-${item.id}-done`"
+      />
+    </div>
+    <template #footer>
+      <NSpace>
+        <NButton type="primary" @click="addTodoItem">
+          <template #icon>
+            <NIcon><AddIcon /></NIcon>
+          </template>
+          Add {{ todo.title }} Item
+        </NButton>
+      </NSpace>
+    </template>
+  </NCard>
 </template>
 
 <script lang="ts">
